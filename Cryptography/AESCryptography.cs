cf48f66fa4405ce1beba417e922204c37406673a
jnaus <jnaus@jjsoft.com>
2011-04-20 10:53:28 -0600

    Cleaned up usings

--- Cryptography/AESCryptography.cs
+++ Cryptography/AESCryptography.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Security.Cryptography;
-using System.IO;
 using System.Diagnostics.Contracts;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace jj.Cryptography
 {
